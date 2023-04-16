4e180b05154a0bf233741f77bf4d46be32c76d93
Masergun <126848167+Masergun@users.noreply.github.com>
2023-04-16 12:35:02 +0300

    Update Size_count.py

--- Size_count.py
+++ Size_count.py
@@ -2,8 +2,8 @@ import os
 file_name = f"{input('Path with file:')}"
 file_size = os.path.getsize(file_name)
 print(f'Bytes:{file_size}')
-print(f'GigaBytes:{file_size //(1024*1024)/1000}')
+print(f'GigaBytes:{file_size //1000//1000//1000}')
 command = "start cmd /K echo "
 for i in range(0,1):
-    command += str(f'GB:{int(file_size //(1024*1024)/1000)}')
+    command += str(f'GB:{int(file_size //1000//1000//1000)}')
 os.system(command)
